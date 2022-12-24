66c5d86896065a62df99e2d07b34e15ed1e43a74
NicoToff <toffolo01@hotmail.com>
2022-12-24 13:06:42 +0100

    Moved connect to its own function to be called when reconnecting ; changed HashMap to Hashtable ; changed lastElem to a Map Entry

--- src/main/kotlin/serialcom/Reader.kt
+++ src/main/kotlin/serialcom/Reader.kt
@@ -4,17 +4,26 @@ import com.fazecast.jSerialComm.SerialPort
 import java.util.*
 
 class Reader(private val comPort: SerialPort) : Thread() {
-    val history: HashMap<Date, Double> = HashMap(256, 0.75f)
-    var lastElem: Double = 0.0
+    val history: Hashtable<Date, Double> = Hashtable(256, 0.75f)
+    var lastElem: Map.Entry<Date, Double>? = null
     private var lastStorageDate: Date = Date()
-    override fun run() {
-        var isOpen = false;
-        while (!isOpen) {
-            isOpen = comPort.openPort()
+    private fun connection() {
+        comPort.closePort()
+        while (!comPort.isOpen) {
+            comPort.openPort()
+            if (!comPort.isOpen) {
+                println("Port ${comPort.systemPortName} is not open, retrying in 0.1 second...")
+                Thread.sleep(100)
+            }
         }
+        println("Port ${comPort.systemPortName} is open!")
         // Set port parameters
         comPort.setComPortParameters(9600, 8, 1, 0)
         comPort.setComPortTimeouts(SerialPort.TIMEOUT_NONBLOCKING, 0, 0)
+    }
+
+    override fun run() {
+        connection()
         // Read data
         try {
             while (true) {
@@ -32,14 +41,19 @@ class Reader(private val comPort: SerialPort) : Thread() {
                     val now = Date()
                     if (now.time - lastStorageDate.time >= 1000) {
                         history[now] = number
-                        lastElem = number
+                        lastElem = history.entries.last()
                         lastStorageDate = now
                         println("Reader: $number")
                     }
                 } catch (nfex: NumberFormatException) {
-                    // This is sometimes thrown when the String parsing into a double fails
+                    // String parsing into a double sometimes fails
                     nfex.printStackTrace()
                     println("Moving on...")
+                } catch (ex: NegativeArraySizeException) {
+                    // Disconnecting the USB port throws this exception
+                    ex.printStackTrace()
+                    println("Reconnecting...")
+                    connection()
                 }
             }
         } catch (ex: Exception) {
