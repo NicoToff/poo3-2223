fb9b887ce2c42a4adf6a915f27671629dcc545e1
NicoToff <toffolo01@hotmail.com>
2023-01-13 17:47:33 +0100

    Removed useless condition

--- src/main/kotlin/serialcom/PortSearch.kt
+++ src/main/kotlin/serialcom/PortSearch.kt
@@ -5,19 +5,17 @@ import com.fazecast.jSerialComm.SerialPort
 
 class PortSearch(private val homePage: HomePage) : Thread() {
     override fun run() {
-        while (true) {
-            if (homePage.availablePorts.isEmpty()) {
-                println("Looking for ports...")
-                homePage.availablePorts = SerialPort.getCommPorts()
-                if (homePage.availablePorts.isNotEmpty()) {
-                    println("Found port: ${homePage.availablePorts[0].systemPortName}")
-                    for (port in homePage.availablePorts) {
-                        homePage.cmbPort.addItem(port.systemPortName)
-                        homePage.cmbPort.selectedItem = port.systemPortName
-                    }
+        while (homePage.availablePorts.isEmpty()) {
+            println("Looking for ports...")
+            homePage.availablePorts = SerialPort.getCommPorts()
+            if (homePage.availablePorts.isNotEmpty()) {
+                println("Found port: ${homePage.availablePorts[0].systemPortName}")
+                for (port in homePage.availablePorts) {
+                    homePage.cmbPort.addItem(port.systemPortName)
+                    homePage.cmbPort.selectedItem = port.systemPortName
                 }
-                Thread.sleep(500)
             }
+            Thread.sleep(500)
         }
     }
 }
